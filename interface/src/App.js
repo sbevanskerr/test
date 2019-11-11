1479634608ca06d9b848d87214050b1541435efa
rCarter101 <xorn101@ufl.edu>
2019-11-10 20:13:25 -0500

    Completed prototype of the thing I was supposed to do

---
+++ interface/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <td><a href="http://google.com"><img src="utensils.png" alt="health" width="72" height="72"></img><pre>
+          Food</pre></a></td>
+          <td><a href="http://google.com"><img src="tornado.png" width="72" height="72"></img><pre>
+          Emergency</pre><pre>
+          Shelter</pre></a></td>
+          <td><a href="http://google.com"><img src="briefcase-medical.png" width="72" height="72"></img><pre>
+          Health</pre></a></td>
+        </tr>
+        <tr>
+          <td><a href="http://google.com"><img src="address-card.png" width="72" height="72"></img><pre>
+          Job</pre></a></td>
+          <td><a href="http://google.com"><img src="school.png" width="72" height="72"></img><pre>
+          Education</pre></a></td>
+          <td><a href="http://google.com"><img src="money-bill-alt.png" width="72" height="72"></img><pre>
+          Financial</pre></a></td>
+        </tr> 
+        <tr>
+          <td><a href="http://google.com"><img src="balance-scale.png" width="72" height="72"></img><pre>
+          Legal</pre></a></td>
+          <td><a href="http://google.com"><img src="paper-plane.png" width="72" height="72"></img><pre>
+          Activities</pre></a></td>
+          <td><a href="http://google.com"><img src="bus-alt.png" width="72" height="72"></img><pre>
+          Transportation</pre></a></td>
+        </tr> 
+        <tr>
+          <td><a href="http://google.com"><img src="home-alt.png" width="72" height="72"></img><pre>
+          Housing</pre></a></td>
+          <td><a href="http://google.com"><img src="calendar-alt.png" width="72" height="72"></img><pre>
+          Life Events</pre></a></td>
+          <td><a href="http://google.com">...</a></td>
+        </tr>   
+      </table>
+    </div>
+  );
+}
+
+export default App;
